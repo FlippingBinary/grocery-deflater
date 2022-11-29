573040f00405283b0244212912de210ff387c4ce
Jon Musselwhite <35066367+FlippingBinary@users.noreply.github.com>
2022-11-29 10:18:48 -0500

    chore: run prettier

--- middleware/src/user.ts
+++ middleware/src/user.ts
@@ -1,19 +1,18 @@
-import { UserModel } from "./db";
-import { User } from "./generated/graphql";
+import { UserModel } from './db'
+import { User } from './generated/graphql'
 
 export async function userResolver({
   email,
 }: {
-  email: string;
+  email: string
 }): Promise<User | null> {
-    const user = await UserModel.findOne({where:{email}});
-    if (!user) return null;
-    return {
-      id: user.id.toString(),
-      name: `${user.firstName} ${user.lastName}`,
-      email: user.email,
-      password: user.password,
-      mobileNumber: user.mobileNumber
-    };
-
+  const user = await UserModel.findOne({ where: { email } })
+  if (!user) return null
+  return {
+    id: user.id.toString(),
+    name: `${user.firstName} ${user.lastName}`,
+    email: user.email,
+    password: user.password,
+    mobileNumber: user.mobileNumber,
+  }
 }
